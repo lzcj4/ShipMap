24dd4ae10538a2e2a58a4bdbe51bb44d01b11b30
NICK\Nick <lzcj4@163.com>
2017-11-30 20:44:48 +0800

    1、载入保存防区

--- map/websocket.py
+++ map/websocket.py
@@ -4,7 +4,7 @@ from channels import Group
 from channels.message import Message
 
 from map.RadarReceiver import RadarReceiver
-from map.wshandler import add_or_update_area
+from map.wshandler import add_or_update_area, get_all_areas
 
 
 class JSONObject:
@@ -12,7 +12,7 @@ class JSONObject:
         self.__dict__ = d
 
 
-WS_CMD = JSONObject({'test': -1, 'area': 0})
+WS_CMD = JSONObject({'connect': 0, 'area': 1})
 
 
 def ws_connect(message: Message):
@@ -25,14 +25,16 @@ def ws_message(message: Message):
     txt = message.content['text']
     print("ws_message:{0}".format(txt))
     txt_obj = json.loads(txt, object_hook=JSONObject)
-    if txt_obj.json != "":
+
+    if txt_obj.cmd == WS_CMD.connect:
+        js_items = get_all_areas()
+        js = {'cmd': WS_CMD.area, "msg": "return areas infos", 'json': js_items}
+        message.reply_channel.send({"text": json.dumps(js)})
+    elif txt_obj.cmd == WS_CMD.area:
         jsonObj = json.loads(txt_obj.json, object_hook=JSONObject)
-        if txt_obj.cmd == WS_CMD.test:
-            pass
-        elif txt_obj.cmd == WS_CMD.area:
-            add_or_update_area(jsonObj)
-        else:
-            pass
+        add_or_update_area(jsonObj)
+    else:
+        pass
 
 
 def ws_disconnect(message: Message):
--- map/wshandler.py
+++ map/wshandler.py
@@ -1,17 +1,25 @@
+import json
+
 from map.models import ShipDefenceArea
 
 
-# from map.websocket import JSONObject
+def get_all_areas():
+    items = ShipDefenceArea.objects.all()
+    result = []
+    for item in items:
+        item_props = {"id": item.name, "longitude": item.longitude, "latitude": item.latitude, "radius": item.radius}
+        result.append(item_props)
+    return json.dumps(result)
 
 
 def add_or_update_area(json_obj):
     items = ShipDefenceArea.objects.filter(name=json_obj.id)
     if len(items) > 0:
-        item = items[0]
-        item.longitude = json_obj.lnglat[0]
-        item.latitude = json_obj.lnglat[1]
-        item.radius = json_obj.radius
-        item.save()
+        for item in items:
+            item.longitude = json_obj.lnglat[0]
+            item.latitude = json_obj.lnglat[1]
+            item.radius = json_obj.radius
+            item.save()
     else:
         ShipDefenceArea.objects.create(name=json_obj.id, longitude=json_obj.lnglat[0],
                                        latitude=json_obj.lnglat[1], radius=json_obj.radius)
